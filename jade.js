87bb9f67d7a86e58c30ac8a4074c900e61d78a39
ehekatlof <pranagy@gmail.com>
2016-09-02 21:17:30 +0000

    jade.js

--- jade.js
+++ jade.js
@@ -3,6 +3,16 @@ var path = require('path');
 
 var app = express();
 
+console.log(process.argv[3]);
+console.log(process.argv[2]);
+
+app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'templates'));
+
+app.get('/home', function(req,res){
+   res.render('index.jade', {date: new Date().toDateString()});
+   res.end();
+});
+
 
 app.listen(process.argv[2]);
