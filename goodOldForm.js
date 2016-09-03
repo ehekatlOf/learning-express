2b1e0ca070fbeb33a96e5dea322a02dd9a206fd1
ehekatlof <pranagy@gmail.com>
2016-09-03 04:50:48 +0000

    goodOldForm

---
+++ goodOldForm.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+console.log(process.argv[2]);
+
+
+
+app.post('/form', function(req,res){
+    console.log(req.body.str);
+    res.end(req.body.str.split('').reverse().join(''));
+});
+
+app.listen(process.argv[2]);
